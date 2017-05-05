d8bae48cf536c2dd1b7640c777c090f4cda1649c
Dan Pozzie <dpozzie@gmail.com>
2017-05-05 17:26:19 -0400

    Creates contact model & generated specs

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :contact do
+    first_name "MyString"
+    last_name "MyString"
+    email_address "MyString"
+    phone_number "MyString"
+    company_name "MyString"
+  end
+end
