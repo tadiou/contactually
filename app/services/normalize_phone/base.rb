348656295f88ca0e6c78183ba2b201eabd9abe5f
Dan Pozzie <dpozzie@gmail.com>
2017-05-07 17:50:51 -0400

    adds strip numbers & remove leading one to NormalizePhone::Base

--- app/services/normalize_phone/base.rb
+++ app/services/normalize_phone/base.rb
@@ -5,12 +5,39 @@ class NormalizePhone
     # Can't use /d because /d matches to certain Arabic numerals which aren't
     #   used in phone numbers. I've spent way too much time handling international
     #   use cases.
-    NUMBERS_ONLY_REGEX = /^[0-9]+$/
+    NUMBERS_ONLY_REGEX = /[0-9]+/
+    MIN_PHONE_DIGITS = 10
 
     def initialize(string)
-
+      @phone_number = NormalizePhone::Extension.split(string).phone_number
     end
 
+    # Enacts normalization procedurally
+    # @param [String]
+    # @return [String]
+    def normalize(phone_number = nil)
+      @phone_number ||= phone_number
+      strip_values!(@phone_number)
+      remove_leading_one!(@phone_number)
+      raise NormalizePhone::NotAPhoneNumberError if phone_number.size < MIN_PHONE_DIGITS
+      phone_number
+    end
 
+    # Strips down a string to only regex [0-9]. Bang method. Replaces in place.
+    # @param [String]
+    # @return [String]
+    def strip_numbers!(phone_number)
+      phone_number.replace(phone_number.scan(NUMBERS_ONLY_REGEX).join)
+    end
+
+    # Removes the leading 1 for all phones if it's exactly
+    #   11 digits loing. Bang method. Replaces in place.
+    # @param [String]
+    # @return [String]
+    def remove_leading_one!(phone_number)
+      return phone_number if phone_number.first != '1' # country code is applicable here
+      return phone_number if phone_number.size != MIN_PHONE_DIGITS + 1
+      phone_number.replace(phone_number.slice(1..-1))
+    end
   end
 end
--- spec/services/normalize_phone/base_spec.rb
+++ spec/services/normalize_phone/base_spec.rb
@@ -1,36 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe NormalizePhone::Base, type: :service do
-  subject(:normalize_phone_service) { described_class.new(phone_number) }
+  let(:not_a_phone_number) { 'This isn\'t a phone number' }
+  let(:phone_number_dashes) { '432-570-6089' }
+  let(:phone_number_parens) { '(153)156-8305' }
+  let(:phone_number_dots) { '218.783.3992' }
+  let(:phone_number_parens_leading_one) { '1-432-570-6089' }
 
-  describe 'Normalizes Phone Number' do
+  describe '.normalize' do
     context 'Not a Phone Number' do
-      let(:phone_number) { 'This isn\'t a phone number' }
-
       it 'raises a NotAPhoneNumber error' do
-        expect { normalize_phone_service }.to raise_error(NotAPhoneNumberError)
       end
     end
 
     context 'Leading 1' do
-      let(:phone_number) { '1-432-570-6089' }
-
       it 'returns a normalized phone number' do
       end
     end
 
     context 'Parentheses' do
-      let(:phone_number) { '(153)156-8305' }
-
       it 'returns a normalized phone number' do
       end
     end
 
     context 'Dots' do
-      let(:phone_number) { '218.783.3992' }
-
       it 'returns a normalized phone number' do
       end
+    end
+  end
+
+  describe '.strip_numbers!' do
+    # usually I'd share these between .normalize  and .strip_numbers! using a shared
+    #   context
+    context 'Not a Phone Number' do
+      it 'raises a NotAPhoneNumber error' do
+        klass = described_class.new(not_a_phone_number)
+        expect(klass.strip_numbers!(klass.phone_number)).to eq('')
+      end
+    end
+
+    context 'Leading 1' do
+      it 'returns a normalized phone number' do
+        klass = described_class.new(phone_number_parens_leading_one)
+        expect(klass.strip_numbers!(klass.phone_number)).to eq('14325706089')
+      end
+    end
+
+    context 'Parentheses' do
+      it 'returns a normalized phone number' do
+      end
+    end
+
+    context 'Dots' do
+      it 'returns a normalized phone number' do
+      end
+    end
+  end
+
+  describe '.remove_leading_one!' do
+    let(:base) { described_class.new(phone_number_parens_leading_one) }
+
+    it 'removes leading 1 if there\'s exactly 11 digits' do
+      klass = described_class.new(phone_number_parens_leading_one)
+      expect(klass.remove_leading_one!('14325706089')).to eq('4325706089')
+    end
+
+    it 'leaves things the way they are if it\'s not a leading 1 and is 11 digits' do
+      klass = described_class.new(phone_number_parens_leading_one)
+      expect(klass.remove_leading_one!('43257060895')).to eq('43257060895')
+    end
+
+    it 'leaves things the way they are if it\'s a leading 1 and is NOT 11 digits' do
+      klass = described_class.new(phone_number_parens_leading_one)
+      expect(klass.remove_leading_one!('1325706089')).to eq('1325706089')
     end
   end
 end
