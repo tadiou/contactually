8a91a3d6fe446c6b11b075d1c9085b50333951dc
Dan Pozzie <dpozzie@gmail.com>
2017-05-07 13:27:34 -0400

    Provided faker data where necessary for Contact factory before we start building tests

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -13,11 +13,16 @@
 #
 
 FactoryGirl.define do
+  # I realize that it's only generating US phones, not international phones,
+  #   but because we're checking against very specific phone number types
+  #   when we're building out the phone normalizer, we'll address them
+  #   as they come instead of relying on random Faker data.
   factory :contact do
-    first_name "MyString"
-    last_name "MyString"
-    email_address "MyString"
-    phone_number "MyString"
-    company_name "MyString"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email_address Faker::Internet.email
+    phone_number Faker::PhoneNumber
+    extension nil
+    company_name Faker::Company.name
   end
 end
