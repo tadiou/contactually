a6db315151301f872cbc1c0bb4b30dada4f30c9a
Dan Pozzie <dpozzie@gmail.com>
2017-05-07 16:39:39 -0400

    adds focus to rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,13 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
